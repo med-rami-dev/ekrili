6a7501bed35f0f13a813fa487af3b60128ca429a
Mohamed Rami <rami63879@gmail.com>
2024-04-05 22:57:02 +0100

    add new google nav bar

--- lib/main_routing_view.dart
+++ lib/main_routing_view.dart
@@ -1,50 +1,65 @@
 import 'package:ekrili/features/home/presentation/view/home_view.dart';
 import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class MainRoutingView extends StatelessWidget {
+class MainRoutingView extends StatefulWidget {
   const MainRoutingView({super.key});
 
   @override
+  State<MainRoutingView> createState() => _MainRoutingViewState();
+}
+
+class _MainRoutingViewState extends State<MainRoutingView> {
+  int selectedIndex = 0;
+  List<Widget> screens = <Widget>[
+    const HomeView(),
+    const Center(
+      child: Text('Search'),
+    ),
+    const Center(
+      child: Text('Process'),
+    ),
+    const Center(
+      child: Text('Profile'),
+    ),
+  ];
+  @override
   Widget build(BuildContext context) {
-    return PersistentTabView(
-      tabs: [
-        PersistentTabConfig(
-          screen: const HomeView(),
-          item: ItemConfig(
-            icon: const Icon(Icons.home),
-            title: "Home",
-          ),
+    return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+        child: GNav(
+          gap: 8,
+          activeColor: Colors.green,
+          tabBackgroundColor: const Color.fromARGB(255, 202, 199, 199),
+          padding: const EdgeInsets.all(15),
+          duration: const Duration(milliseconds: 350),
+          onTabChange: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          tabs: const [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.search,
+              text: 'Search',
+            ),
+            GButton(
+              icon: Icons.refresh,
+              text: 'Process',
+            ),
+            GButton(
+              icon: Icons.person,
+              text: 'Profile',
+            ),
+          ],
         ),
-        PersistentTabConfig(
-          screen: Container(),
-          item: ItemConfig(
-            icon: const Icon(Icons.search),
-            title: "Search",
-          ),
-        ),
-        PersistentTabConfig(
-          screen: Container(),
-          item: ItemConfig(
-            icon: const Icon(Icons.settings),
-            title: "Settings",
-          ),
-        ),
-        PersistentTabConfig(
-          screen: Container(),
-          item: ItemConfig(
-            icon: const Icon(Icons.qr_code),
-            title: "Scan",
-          ),
-        ),
-      ],
-      navBarBuilder: (navBarConfig) => Style1BottomNavBar(
-        navBarConfig: navBarConfig,
       ),
-      screenTransitionAnimation: const ScreenTransitionAnimation(
-        curve: Curves.ease,
-        duration: Duration(milliseconds: 200),
-      ),
+      body: screens[selectedIndex],
     );
   }
 }
